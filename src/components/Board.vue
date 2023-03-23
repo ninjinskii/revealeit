180e29b90f96ce13e6b181582085dd441a14b0d4
ninjinski <louis.bailly.pro@gmail.com>
2023-03-23 21:02:55 +0100

    Add components

---
+++ src/components/Board.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useGlobalStore } from '../stores/store'
+import Slot from './Slot.vue'
+
+const store = useGlobalStore()
+const { board } = store
+
+</script>
+
+<template>
+  <Slot v-for="(slot, index) in board.values" :colored="index % 2 === 0" />
+</template>
---
+++ src/components/Slot.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineProps<{ slot: Slot, colored: boolean }>()
+
+</script>
+
+<template>
+  <div class="root">
+
+  </div>
+</template>
