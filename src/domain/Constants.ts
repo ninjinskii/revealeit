65b8fb29317855df331fa69a84119e7318828af8
ninjinski <louis.bailly.pro@gmail.com>
2023-03-26 23:11:55 +0200

    chore: restore backend url

--- src/domain/Constants.ts
+++ src/domain/Constants.ts
@@ -8,8 +8,7 @@ export class Constants {
   ];
   public static PLAYER_ID_KEY = "revealeit.player.id";
   public static PLAYER_ID_NAME = "revealeit.player.name";
-  public static SERVER_WEBSOCKET_URL =
-    "ws://revealeit-backend.njk.localhost:5000/"; // "wss://revealeit-backend.deno.dev"
+  public static SERVER_WEBSOCKET_URL = "wss://revealeit-backend.deno.dev"
   public static MESSAGE_HANDSHAKE_KEY = "handshake";
   public static MESSAGE_MOVE_KEY = "move";
   public static MESSAGE_KILL_KEY = "kill";
